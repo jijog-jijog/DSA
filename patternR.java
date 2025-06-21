e5f63d594965cfbc2058f6e818f3854cbfa237f7
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-06-21 22:00:48 +0530

    Add patternR.java for a new pattern printing implementation

---
+++ patternR.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class patternR {
+    public static void main(String[] args) {
+        System.out.print("Enter the number want to print : ");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        for(int row =1; row<=(n*2)-1; row++)
+        {
+            {
+                for(int col = 1; col<=(n*2)-1; col++){
+                    int topdis = row, downdis = (n*2) - row, lefdis = col, rigdis = (n*2) - col;
+                    int min = Math.min(Math.min(topdis, downdis), Math.min(lefdis, rigdis));
+                    System.out.print(n-min+1 + "  ");
+                }
+            }
+        System.out.println();
+        }
+
+    }
+}
+            
+   
